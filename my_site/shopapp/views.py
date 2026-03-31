0777a8d819f7c92aa8cf8978ed29c5d8660b7b2c
Nikita_osip <osipov.nik.ev@gmail.com>
2026-03-31 20:14:40 +0300

    Change shopapp, add html site

--- my_site/shopapp/views.py
+++ my_site/shopapp/views.py
@@ -1,3 +1,18 @@
+import datetime
+from timeit import default_timer
+
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+def shop_index(request: HttpRequest):
+    products = [
+        ("Laptop", 1999),
+        ("Desctop", 2999),
+        ("Smartphone", 999),
+    ]
+    context = {
+        "date": datetime.datetime.now(),
+        "time_running": default_timer(),
+        "products": products,
+    }
+    return render(request, "shopapp/shop_index.html", context=context)
